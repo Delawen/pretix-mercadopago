c79dbe0c40c5392abb985af34d94b9fab7430fe2
Manuel Retamozo <manuel@retamozo.com.ar>
2020-07-12 13:57:29 -0300

    register provider

--- pretix_mercadopago/signals.py
+++ pretix_mercadopago/signals.py
@@ -1,1 +1,10 @@
 # Register your receivers here
+from django.dispatch import receiver
+
+from pretix.base.signals import register_payment_providers
+
+
+@receiver(register_payment_providers, dispatch_uid="payment_mercadopago")
+def register_payment_provider(sender, **kwargs):
+    from .payment import Mercadopago
+    return Mercadopago
