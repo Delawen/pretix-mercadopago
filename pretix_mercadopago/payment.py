36076e3bd323b95e7a83f99dc91122ca0b905b3a
Maria Arias de Reyna <ariasdereyna@redhat.com>
2020-07-28 19:26:52 +0200

    Cleanup

--- pretix_mercadopago/payment.py
+++ pretix_mercadopago/payment.py
@@ -1,25 +1,19 @@
-import json
 import logging
-import urllib.parse
 from collections import OrderedDict
 from decimal import Decimal
 
 import mercadopago
-from mercadopago import MP
             
 from django import forms
 from django.contrib import messages
-from django.core import signing
 from django.http import HttpRequest
 from django.template.loader import get_template
 from django.urls import reverse
 from django.utils.translation import gettext as __, gettext_lazy as _
 from i18nfield.strings import LazyI18nString
 
-from pretix.base.decimal import round_decimal
 from pretix.base.models import Event, Order, OrderPayment, OrderRefund, Quota
 from pretix.base.payment import BasePaymentProvider, PaymentException
-#from pretix.base.services.mail import SendMailException
 from pretix.base.settings import SettingsSandbox
 from pretix.helpers.urls import build_absolute_uri as build_global_uri
 from pretix.multidomain.urlreverse import build_absolute_uri
@@ -36,6 +30,14 @@ SUPPORTED_CURRENCIES = ['ARS', 'BRL', 'CLP','MXN','COP','PEN','UYU']
 # UYU Peso uruguayo.
 
 LOCAL_ONLY_CURRENCIES = ['ARS']
+
+
+def payment_partial_refund_supported(payment: OrderPayment):
+    return False
+
+
+def payment_refund_supported(payment: OrderPayment):
+    return False
 
 
 class Mercadopago(BasePaymentProvider):
@@ -299,96 +301,6 @@ class Mercadopago(BasePaymentProvider):
         ctx = {'request': request, 'event': self.event, 'settings': self.settings}
         return template.render(ctx)
 
-    def execute_payment(self, request: HttpRequest, payment: OrderPayment):
-        if (request.session.get('payment_paypal_id', '') == '' or request.session.get('payment_paypal_payer', '') == ''):
-            raise PaymentException(_('We were unable to process your payment. See below for details on how to '
-                                     'proceed.'))
-
-        self.init_api()
-        pp_payment = paypalrestsdk.Payment.find(request.session.get('payment_paypal_id'))
-        ReferencedPayPalObject.objects.get_or_create(order=payment.order, payment=payment, reference=pp_payment.id)
-        if str(pp_payment.transactions[0].amount.total) != str(payment.amount) or pp_payment.transactions[0].amount.currency \
-                != self.event.currency:
-            logger.error('Value mismatch: Payment %s vs paypal trans %s' % (payment.id, str(pp_payment)))
-            raise PaymentException(_('We were unable to process your payment. See below for details on how to '
-                                     'proceed.'))
-
-        return self._execute_payment(pp_payment, request, payment)
-
-    def _execute_payment(self, payment, request, payment_obj):
-        if payment.state == 'created':
-            payment.replace([
-                {
-                    "op": "replace",
-                    "path": "/transactions/0/item_list",
-                    "value": {
-                        "items": [
-                            {
-                                "name": __('Order {slug}-{code}').format(slug=self.event.slug.upper(),
-                                                                         code=payment_obj.order.code),
-                                "quantity": 1,
-                                "price": self.format_price(payment_obj.amount),
-                                "currency": payment_obj.order.event.currency
-                            }
-                        ]
-                    }
-                },
-                {
-                    "op": "replace",
-                    "path": "/transactions/0/description",
-                    "value": __('Order {order} for {event}').format(
-                        event=request.event.name,
-                        order=payment_obj.order.code
-                    )
-                }
-            ])
-            try:
-                payment.execute({"payer_id": request.session.get('payment_paypal_payer')})
-            except Exception as e:
-                messages.error(request, _('We had trouble communicating with PayPal'))
-                logger.exception('Error on creating payment: ' + str(e))
-
-        for trans in payment.transactions:
-            for rr in trans.related_resources:
-                if hasattr(rr, 'sale') and rr.sale:
-                    if rr.sale.state == 'pending':
-                        messages.warning(request, _('PayPal has not yet approved the payment. We will inform you as '
-                                                    'soon as the payment completed.'))
-                        payment_obj.info = json.dumps(payment.to_dict())
-                        payment_obj.state = OrderPayment.PAYMENT_STATE_PENDING
-                        payment_obj.save()
-                        return
-
-        payment_obj.refresh_from_db()
-        if payment.state == 'pending':
-            messages.warning(request, _('PayPal has not yet approved the payment. We will inform you as soon as the '
-                                        'payment completed.'))
-            payment_obj.info = json.dumps(payment.to_dict())
-            payment_obj.state = OrderPayment.PAYMENT_STATE_PENDING
-            payment_obj.save()
-            return
-
-        if payment.state != 'approved':
-            payment_obj.fail(info=payment.to_dict())
-            logger.error('Invalid state: %s' % str(payment))
-            raise PaymentException(_('We were unable to process your payment. See below for details on how to '
-                                     'proceed.'))
-
-        if payment_obj.state == OrderPayment.PAYMENT_STATE_CONFIRMED:
-            logger.warning('PayPal success event even though order is already marked as paid')
-            return
-
-        try:
-            payment_obj.info = json.dumps(payment.to_dict())
-            payment_obj.save(update_fields=['info'])
-            payment_obj.confirm()
-        except Quota.QuotaExceededException as e:
-            raise PaymentException(str(e))
-
-        except SendMailException:
-            messages.warning(request, _('There was an error sending the confirmation mail.'))
-        return None
-
     def payment_pending_render(self, request, payment) -> str:
         retry = True
         try:
@@ -423,59 +335,15 @@ class Mercadopago(BasePaymentProvider):
             "sale_id": sale_id,
         }
 
-    def payment_control_render(self, request: HttpRequest, payment: OrderPayment):
-        template = get_template('pretixplugins/paypal/control.html')
-        sale_id = None
-        for trans in payment.info_data.get('transactions', []):
-            for res in trans.get('related_resources', []):
-                if 'sale' in res and 'id' in res['sale']:
-                    sale_id = res['sale']['id']
-        ctx = {'request': request, 'event': self.event, 'settings': self.settings,
-               'payment_info': payment.info_data, 'order': payment.order, 'sale_id': sale_id}
-        return template.render(ctx)
-
-    def payment_partial_refund_supported(self, payment: OrderPayment):
-        return False
-
-    def payment_refund_supported(self, payment: OrderPayment):
-        return False
-
     def execute_refund(self, refund: OrderRefund):
         raise PaymentException(_('Refunding is not supported.'))
 
     def __payment_prepare(self, request, payment_obj):
         self.init_api()
 
-        if request.event.settings.payment_paypal_connect_user_id:
-            try:
-                tokeninfo = Tokeninfo.create_with_refresh_token(request.event.settings.payment_paypal_connect_refresh_token)
-            except BadRequest as ex:
-                ex = json.loads(ex.content)
-                messages.error(request, '{}: {} ({})'.format(
-                    _('We had trouble communicating with PayPal'),
-                    ex['error_description'],
-                    ex['correlation_id'])
-                )
-                return
+        payee = {}
 
-            # Even if the token has been refreshed, calling userinfo() can fail. In this case we just don't
-            # get the userinfo again and use the payment_paypal_connect_user_id that we already have on file
-            try:
-                userinfo = tokeninfo.userinfo()
-                request.event.settings.payment_paypal_connect_user_id = userinfo.email
-            except UnauthorizedAccess:
-                pass
-
-            payee = {
-                "email": request.event.settings.payment_paypal_connect_user_id,
-                # If PayPal ever offers a good way to get the MerchantID via the Identifity API,
-                # we should use it instead of the merchant's eMail-address
-                # "merchant_id": request.event.settings.payment_paypal_connect_user_id,
-            }
-        else:
-            payee = {}
-
-#Agrego mercadopago order hardcode para testear
+        #Agrego mercadopago order hardcode para testear
         preference = {
               "items": [
                 {
@@ -531,54 +399,21 @@ class Mercadopago(BasePaymentProvider):
         request.session['payment_mercadopago_payment'] = payment_obj.pk
         return self._create_payment(request, preferenceResult)
 
-    def shred_payment_info(self, obj):
-        if obj.info:
-            d = json.loads(obj.info)
-            new = {
-                'id': d.get('id'),
-                'payer': {
-                    'payer_info': {
-                        'email': '█'
-                    }
-                },
-                'update_time': d.get('update_time'),
-                'transactions': [
-                    {
-                        'amount': t.get('amount')
-                    } for t in d.get('transactions', [])
-                ],
-                '_shredded': True
-            }
-            obj.info = json.dumps(new)
-            obj.save(update_fields=['info'])
-
-        for le in obj.order.all_logentries().filter(action_type="pretix.plugins.paypal.event").exclude(data=""):
-            d = le.parsed_data
-            if 'resource' in d:
-                d['resource'] = {
-                    'id': d['resource'].get('id'),
-                    'sale_id': d['resource'].get('sale_id'),
-                    'parent_payment': d['resource'].get('parent_payment'),
-                }
-            le.data = json.dumps(d)
-            le.shredded = True
-            le.save(update_fields=['data', 'shredded'])
-
     def render_invoice_text(self, order: Order, payment: OrderPayment) -> str:
         if order.status == Order.STATUS_PAID:
             if payment.info_data.get('id', None):
                 try:
                     return '{}\r\n{}: {}\r\n{}: {}'.format(
                         _('The payment for this invoice has already been received.'),
-                        _('PayPal payment ID'),
+                        _('Payment ID'),
                         payment.info_data['id'],
-                        _('PayPal sale ID'),
+                        _('Sale ID'),
                         payment.info_data['transactions'][0]['related_resources'][0]['sale']['id']
                     )
                 except (KeyError, IndexError):
                     return '{}\r\n{}: {}'.format(
                         _('The payment for this invoice has already been received.'),
-                        _('PayPal payment ID'),
+                        _('Payment ID'),
                         payment.info_data['id']
                     )
             else:
